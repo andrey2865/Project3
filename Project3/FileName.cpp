36fb680d1c6768c58e9aef55996d5f87c3858f1f
andrey2865 <andrew.lipa15@gmail.com>
2024-12-23 05:28:58 +0500

    Update FileName.cpp

--- Project3/FileName.cpp
+++ Project3/FileName.cpp
@@ -12,43 +12,56 @@ double f_proizv(double x) {
 double g(double x) {
     return cos(sqrt(1-0.3*x*x*x));
 }
-double newton_method(double tol = 1e-6, int max_iter = 100) {
+double newton_method(double e = 1e-6, int max_iter = 100) {
     double x = 0.99;
     for (int i = 0; i < max_iter; ++i) {
         double fx = f(x);
         double fpr = f_proizv(x);
         double x_next = x - fx / fpr;
         
-        if (fabs(x_next - x) < tol) {
+        if (fabs(x_next - x) < e) {
             return x_next;
         }
         else {
-            cout << "итерация " << i << ": x = " << x_next<<endl;
+            cout << "итерация " << i+1 << ": x = " << x_next<<endl;
         }
         x = x_next;
     }
 }
 
-double iteration_method(double tol = 1e-6, int max_iter = 100) {
+double iteration_method(double e = 1e-6, int max_iter = 100) {
     double x = 0;
     for (int i = 0; i < max_iter; ++i) {
         double x_next = g(x);
-        if (fabs(x_next - x) < tol) {
+        if (fabs(x_next - x) < e) {
             return x_next;
         }
         else {
-            cout << "итерация " << i << ": x = " << x_next << endl;
+            cout << "итерация " << i+1 << ": x = " << x_next << endl;
         }
 
         x = x_next;
     }
 }
-
+double halfdel_method(double a, double b, double e = 1e-6) {
+    double x = (a + b) / 2.0;
+    cout <<"Приближение: x = " << x << endl;
+    if (fabs((a - b)) < e) {
+        return (a + b) / 2.0;
+    }
+    else if (f(a)*f(x)<0){
+        return halfdel_method(a, x);
+    }
+    else {
+        return halfdel_method(x, b);
+    }
+}
 int main() {
     double root_newton = newton_method();
     cout << "Корень уравнения методом Ньютона: " << root_newton << endl;
     double root_iteration = iteration_method();
     cout << "Корень уравнения методом итераций: " << root_iteration << endl;
+    cout << "Корень уравнения методом половинного деления: " << halfdel_method(0,1) << endl;
     system("pause");
     return 0;
 }
